a7ae9959bbe5699b9d8ae2f118119fcb23f7f57e
java_216 <jiangbin12032430@126.com>
2019-10-29 11:53:02 +0800

    初始graph项目

---
+++ src/main/java/com/jiang/graph/controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.jiang.graph.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseController {
+	protected Logger log = LoggerFactory.getLogger(getClass());
+}
---
+++ src/main/java/com/jiang/graph/service/IQuestionService.java
@@ -0,0 +1,7 @@
+package com.jiang.graph.service;
+
+public interface IQuestionService {
+
+	  void showDictPath();
+	  String answer(String question) throws Exception;
+}
